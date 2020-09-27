72cef639d150935a7e4bb8340202d08ca9b382e7
way <way>
2020-09-27 14:14:51 +0800

    update ownthink

--- demo_ownthink.py
+++ demo_ownthink.py
@@ -7,27 +7,27 @@
 
 import requests
 
+
 class Robot:
 
-    def __init__(self, appid):
-        self.appid = appid
+    def __init__(self, user):
+        self.user = user
 
-    def reply(self, user, spoken):
-        url = f'https://api.ownthink.com/bot?appid=xiaosi&userid={user}&spoken={spoken}'
+    def reply(self, spoken):
+        url = f'https://api.ownthink.com/bot?appid=xiaosi&userid={self.user}&spoken={spoken}'
         response = requests.get(url).json()
         if response['message'] == 'success':
             return response['data']['info']['text']
         else:
             return "你说什么，风太大，我听不见"
 
+
 if __name__ == '__main__':
-    fatboy = '0b519464452ebad4fcae4a59f05aa750'
-    mumu = '00db9faed5c1a06cfdecb892ba1e9b6e'
-    robot_fatboy = Robot(fatboy)
-    robot_mumu = Robot(mumu)
+    robot_fatboy = Robot('fatboy20200927')
+    robot_mumu = Robot('mumu20200927')
     spoken = '你好'
     while 1:
-        spoken = robot_fatboy.reply(mumu, spoken)
+        spoken = robot_fatboy.reply(spoken)
         print(f'fatboy: {spoken}')
-        spoken = robot_mumu.reply(fatboy, spoken)
+        spoken = robot_mumu.reply(spoken)
         print(f'mumu: {spoken}')
