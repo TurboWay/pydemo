70be89f5ca0bc8299ed7b0e38346d6f6c115f51b
way <way>
2021-01-25 18:03:06 +0800

    add file parse

---
+++ demo_file_parse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/1/25 14:14
+# @Author : way
+# @Site : 
+# @Describe: docx , pdf 文件解析
+
+import base64
+import docx
+from io import BytesIO
+
+filename = r"C:\Users\Administrator\Desktop\1.docx"
+# with open(filename, 'rb') as f:
+#     file = BytesIO(f.read())
+# with open(filename, 'rb') as f:
+#     st = base64.b64encode(f.read())
+#     file = BytesIO(base64.b64decode(st))
+doc = docx.Document(filename)
+paras = [para.text.replace('\n', ';').replace('\t', ' ') for para in doc.paragraphs]
+content = ';'.join(paras)
+print(content)
+
+
+import pdfplumber
+
+filename = r"C:\Users\Administrator\Desktop\8E06BBABF68CA632DC0820E4DC110E4E.pdf"
+# with open(filename, 'rb') as f:
+#     file = BytesIO(f.read())
+# with open(filename, 'rb') as f:
+#     st = base64.b64encode(f.read()).decode()
+#     file = BytesIO(base64.b64decode(st))
+pdf = pdfplumber.open(filename)
+paras = [page.extract_text().replace('\n', ';').replace('\t', ' ') for page in pdf.pages]
+content = ';'.join(paras)
+print(content)
+
+# doc 解析, win 可以使用 win32com 先把 doc 转 docx ; centos 可以使用 antiword
