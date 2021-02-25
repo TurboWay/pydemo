4a47b0137d2858e42ea610f1a8bfbc408b763e4f
way <way>
2021-02-25 15:17:56 +0800

    多线程+进度条

--- demo_multithread.py
+++ demo_multithread.py
@@ -3,32 +3,78 @@
 # @Time : 2020/8/10 14:59
 # @Author : way
 # @Site : 
-# @Describe: 多线程并发
+# @Describe: 多线程并发 + 进度条
 
 import time
-from concurrent.futures import ThreadPoolExecutor
+import queue
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def test1(val1, val2):
-    print(val1 + val2)
-    return True
+# 多线程 + map (无返回结果)
+def threads_map():
+    def func(i):
+        time.sleep(2)
+        print(i)
+
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        pool.map(func, range(50))
+        pool.shutdown(wait=True)
 
 
-def test2(val):
-    time.sleep(3)
-    print(val)
+# 多线程 + map (多参数、有返回结果)
+def threads_map_return():
+    def func(a, b):
+        time.sleep(2)
+        return a + b
+
+    targets = [
+        (1, 5), (7, 8), (6, 8)
+    ]
+
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        for s in pool.map(func, *zip(*targets)):
+            print(s)
 
 
-targets = [
-    (4, 5),
-    (6, 7)
-]
+# 多线程 + 进度条
+def threads_tqdm():
+    def func(a):
+        time.sleep(a)
 
-num = 10
-pool = ThreadPoolExecutor(max_workers=num)
-for result in pool.map(test1, *zip(*targets)):
-    ...
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        tasks = [pool.submit(func, i) for i in range(10)]
+        for _ in tqdm(as_completed(tasks), desc='处理中...', total=len(tasks)):
+            ...
 
-pool = ThreadPoolExecutor(max_workers=num)
-for result in pool.map(test2, range(50)):
-    ...
+
+# 多线程 + 进度条(有返回值)
+def threads_tqdm_return():
+    def func(a, b):
+        time.sleep(2)
+        return a + b
+
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        tasks = [pool.submit(func, *(i, i)) for i in range(50)]
+        for r in tqdm(as_completed(tasks), desc='处理中...', total=len(tasks)):
+            print(r.result())
+
+
+# 重写 ThreadPoolExecutor 增加队列大小限制
+class BoundThreadPoolExecutor(ThreadPoolExecutor):
+
+    def __init__(self, max_queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._work_queue = queue.Queue(max_queue)
+
+
+# 多线程(任务数很大时，限制线程池队列大小，防止内存飙升)
+def threads_queue():
+    def func(i):
+        print(i)
+
+    num = 10
+    with BoundThreadPoolExecutor(max_queue=num * 2, max_workers=num) as pool:
+        for i in range(1000000):
+            pool.submit(func, i)
+        pool.shutdown(wait=True)
