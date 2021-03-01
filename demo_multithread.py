1f7c940bdb10ae0131704487223dbd723272f0c3
way <way>
2021-03-01 14:50:39 +0800

    bug fix

--- demo_multithread.py
+++ demo_multithread.py
@@ -10,11 +10,22 @@ import queue
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+"""
+1、使用 map 时：
+   返回结果就是函数的返回值，
+   如果函数报错，则未运行的函数队列会自动取消
+   
+2、使用 submit 提交时
+   as_completed 返回的是对象 r ，需要调用 r.result() 获取结果，通过 r.exception() 判断函数是否报错，
+   剩余未运行的函数队列会继续运行，需要通过 task.cancel() 取消
+"""
+
+
 
 # 多线程 + map (无返回结果)
 def threads_map():
     def func(i):
-        time.sleep(2)
+        time.sleep(i)
         print(i)
 
     with ThreadPoolExecutor(max_workers=10) as pool:
@@ -46,19 +57,22 @@ def threads_tqdm():
         tasks = [pool.submit(func, i) for i in range(10)]
         for r in tqdm(as_completed(tasks), desc='处理中...', total=len(tasks)):
             if r.exception():
+                for task in reversed(tasks):
+                    task.cancel()
                 raise r.exception()
 
 
 # 多线程 + 进度条(有返回值)
 def threads_tqdm_return():
     def func(a, b):
-        time.sleep(2)
         return a + b
 
     with ThreadPoolExecutor(max_workers=10) as pool:
         tasks = [pool.submit(func, *(i, i)) for i in range(50)]
         for r in tqdm(as_completed(tasks), desc='处理中...', total=len(tasks)):
             if r.exception():
+                for task in reversed(tasks):
+                    task.cancel()
                 raise r.exception()
             print(r.result())
 
