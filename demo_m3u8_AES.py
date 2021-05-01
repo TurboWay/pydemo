15c31dba4da2e921d9366f315d3fdcc5f5461207
way <way>
2021-05-01 20:00:36 +0800

    update

--- demo_m3u8_AES.py
+++ demo_m3u8_AES.py
@@ -6,29 +6,39 @@
 # @Describe: m3u8 下载器 (AES加密)
 
 import os
+import shutil
 import re
 import requests
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from Crypto.Cipher import AES
-
+from urllib.parse import urlparse, urljoin
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
 
-key = '92c4749ace9c8bba'
-cryptor = AES.new(key.encode('utf-8'), AES.MODE_CBC)
 
 class m3u8:
 
-    def __init__(self, dir, num=10):
+    def __init__(self, url, key, dir, target, num=10):
         """
-        :param dir: 下载路径
+        :param url: m3u8 资源链接
+        :param key: aes 加密key
+        :param dir: 文件夹名称
+        :param target: 文件名
         :param num: 并发数
         """
         self.dir = dir
         self.num = num
+        self.url = url
+        self.target = target.split('.')[0] + '.mp4'
+        self.temp_dir = os.path.join(self.dir, target.split('.')[0] + '_temp')
+        os.makedirs(self.dir, exist_ok=True)
+        os.makedirs(self.temp_dir, exist_ok=True)
+        self.cryptor = AES.new(key.encode('utf-8'), AES.MODE_CBC) if key else None
+        parse = urlparse(self.url)
+        self.host = f"{parse.scheme}://{parse.netloc}"
 
     @staticmethod
     def sort(px):
@@ -38,26 +48,28 @@ class m3u8:
                 break
         return px
 
-    @staticmethod
-    def download_ts(path, ts_url):
+    def download_ts(self, path, ts_url):
         if os.path.exists(path):
             return
         path_loading = f'{path}.loading'
         try:
             with open(path_loading, 'wb') as f:
                 data = requests.get(ts_url, headers=headers, stream=True, timeout=60).content
-                f.write(cryptor.decrypt(data))
+                if self.cryptor:
+                    data = self.cryptor.decrypt(data)
+                f.write(data)
             os.rename(path_loading, path)
         except:
             ...
 
     def get_urls(self):
+        res = requests.get(self.url, headers=headers).text
         urls = []
-        with open('魔女m3u8.txt', 'r') as f:
-            for px, ts_url in enumerate(re.findall('(.*?js)', f.read())):
-                path = f'{dir}/{self.sort(px)}.ts'
-                urls.append((path, ts_url))
-            return urls
+        for px, ts_url in enumerate(re.findall('(.*?\.ts)', res)):
+            ts_url = urljoin(self.host, ts_url)
+            path = f'{self.temp_dir}/{self.sort(px)}.ts'
+            urls.append((path, ts_url))
+        return urls
 
     def download(self):
         urls = self.get_urls()
@@ -67,19 +79,29 @@ class m3u8:
                 ...
 
     def merge(self):
-        os.chdir(self.dir)
+        os.chdir(self.temp_dir)
         for path in os.listdir():
             if path.endswith('loading'):
                 raise Exception("部分文件未下载成功, 重新执行")
 
-        os.system("copy /b *ts movie.mp4")
-        for path in os.listdir():
-            if path.endswith('.ts'):
-                os.remove(path)
+        os.system(f"copy /b *ts {self.target}")  # 合并 ts
+        tr = os.path.join(self.dir, self.target)
+        if os.path.exists(tr):
+            os.remove(tr)
+        shutil.move(self.target, self.dir)
+        os.chdir(self.dir)
+        shutil.rmtree(self.temp_dir)  # 移除临时文件夹
+        print(f"{self.target} 下载完成！")
 
 
 if __name__ == "__main__":
-    dir = f'{os.getcwd()}/files/魔女'
-    m = m3u8(dir, 20)
-    m.download()
-    m.merge()
+    dir = f'{os.getcwd()}/files/猎鹰与冬兵'
+    targets = [
+        ('https://mhcdn.mhqiyi.com/20210326/7Mvnme5R/1000kb/hls/index.m3u8', '6ae169d7dbacab8e', '猎鹰与冬兵第02集',),
+        ('https://mhcdn.mhqiyi.com/20210402/ICU35rhY/1000kb/hls/index.m3u8', '68fa55edaebdbdbc', '猎鹰与冬兵第03集',),
+    ]
+    for tr in targets:
+        url, key, target = tr
+        m = m3u8(url, key, dir, target, 20)
+        m.download()
+        m.merge()
--- demo_onepiece.py
+++ demo_onepiece.py
@@ -5,66 +5,24 @@
 # @Site : 
 # @Describe: 海贼王漫画下载
 
-import sys
 import os
-import time
-import urllib3
 import requests
-from selenium import webdriver
+from bs4 import BeautifulSoup
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-class OnePiece:
-
-    def __init__(self):
-        # 启动浏览器
-        chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')  # 无头浏览
-        # chrome_options.add_argument('--disable-gpu')  # 禁用gpu加速
-        chrome_options.add_argument('--test-type --ignore-certificate-errors')  # 关闭https验证
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.driver.maximize_window()
-        self.driver.get('https://manhua.fzdm.com')
-        time.sleep(2)
-
-    @staticmethod
-    def downpic(url, path):
-        for _ in range(10):
-            try:
-                with open(path, 'wb') as f:
-                    f.write(requests.get(url, ).content)
-                    print(path + '下载成功')
-                    return
-            except:
-                ...
-        print(path + '下载失败：' + url)
-
-    def down(self, chapternum):
-        dir = f'海賊王/{chapternum}'
-        os.makedirs(dir, exist_ok=True)
-        for i in range(100):
-            url = f'https://manhua.fzdm.com/2/{chapternum}/index_{i}.html'
-            self.driver.get(url)
-            time.sleep(2)
-            try:
-                img = self.driver.find_element_by_xpath('//*[@id="mhpic"]').get_attribute('src')
-            except:
-                img = None
-            if img:
-                path = f'{dir}/{i + 1}.jpg'
-                self.downpic(img, path)
-            else:
-                print(f"海賊王第 {chapternum} 话下载完成")
-                return
-
+def down(num, wxurl):
+    dir = f'{os.getcwd()}/files/海賊王/{num}'
+    os.makedirs(dir, exist_ok=True)
+    res = requests.get(wxurl)
+    imgs = BeautifulSoup(res.text, 'lxml').find('div', id="js_content").find_all('img')
+    for px, img in enumerate(imgs, 1):
+        url = img.get("data-src")
+        # print(url)
+        path = os.path.join(dir, f'{px}.jpg')
+        with open(path, 'wb') as f:
+            f.write(requests.get(url).content)
+    print(f"{dir} 下载完成！")
 
 if __name__ == "__main__":
-    chapternum = 1000
-
-    if len(sys.argv) > 2:
-        chapternum = sys.argv[1]
-
-    op = OnePiece()
-    op.down(chapternum)
-    op.driver.quit()
+    num = 1011
+    wxurl = 'https://mp.weixin.qq.com/s/VoIwzJKPRDjVtMqI0YcXYg'
+    down(num, wxurl)
