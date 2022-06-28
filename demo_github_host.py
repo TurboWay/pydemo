604773187c945be1b36b74a9d258124967ddc711
way <way>
2022-06-28 09:07:29 +0800

    update

--- demo_github_host.py
+++ demo_github_host.py
@@ -5,41 +5,24 @@
 # @Site : 
 # @Describe: 更新 github 的 host 配置
 
-import re
+import subprocess
 import requests
 
-github_hosts = """# GitHub Start
-ip1 assets-cdn.github.com
-ip2 user-images.githubusercontent.com
-ip2 raw.githubusercontent.com
-ip2 gist.githubusercontent.com
-ip2 cloud.githubusercontent.com
-ip2 camo.githubusercontent.com
-ip2 avatars.githubusercontent.com
-ip2 avatars0.githubusercontent.com
-ip2 avatars1.githubusercontent.com
-ip2 avatars2.githubusercontent.com
-ip2 avatars3.githubusercontent.com
-ip2 avatars4.githubusercontent.com
-ip2 avatars5.githubusercontent.com
-ip2 avatars6.githubusercontent.com
-ip2 avatars7.githubusercontent.com
-ip2 avatars8.githubusercontent.com
-# GitHub End
-"""
-
-ip_map = {
-    'ip1': 'https://github.com.ipaddress.com/assets-cdn.github.com',
-    'ip2': 'https://githubusercontent.com.ipaddress.com/raw.githubusercontent.com',
-}
-
-for key, url in ip_map.items():
-    ip = re.findall('"comma-separated"><li>(.*?)</li>', requests.get(url).text)[0]
-    github_hosts = github_hosts.replace(key, ip)
+url = 'https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts'
+res = requests.get(url)
+github_hosts = '\n'.join([i for i in res.text.strip().split('\n') if 'github' in i.lower() and not i.startswith('#') or 'Update at' in i])
 
 path = "C:\Windows\System32\drivers\etc\hosts"
+bak_path = "C:\Windows\System32\drivers\etc\hosts_bak"
 with open(path, 'r', encoding='utf-8') as f:
-    new = ''.join([line for line in f.readlines() if 'github' not in line.lower()]) + github_hosts
-    with open('hosts', 'w', encoding='utf-8') as f2:
+    # 先备份
+    old = ''.join([line for line in f.readlines() if 'github' not in line.lower()])
+    with open(bak_path, 'w', encoding='utf-8') as bak:
+        bak.write(old)
+    # 更新host
+    new = old + f'# GitHub Start\n{github_hosts}\n# GitHub End'
+    with open(path, 'w', encoding='utf-8') as f2:
          f2.write(new)
-print(f"github host 已更新，自行替换到 {path[:-6]}")
+print(f"github host 已更新")
+child_process = subprocess.Popen('ipconfig /flushdns', shell=True)
+(stdout, stderr) = child_process.communicate()
